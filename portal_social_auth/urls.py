155e5b2c20db61a61a0c59e3037d425cc855b59d
anaco <ndedaanaco@gmail.com>
2020-12-28 17:43:33 +0300

    j

--- portal_social_auth/urls.py
+++ portal_social_auth/urls.py
@@ -21,5 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('',index,name='index'),
+    path('index',index,name='index'),
+
     path('social-auth', include('social_django.urls', namespace='social')),
 ]
