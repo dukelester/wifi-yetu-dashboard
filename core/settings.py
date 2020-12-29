07dc2c2ab539570c8206ce95767d3492ee2b83ab
anaco <ndedaanaco@gmail.com>
2020-12-29 13:36:55 +0300

    yeey

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = True
 # DEBUG = False
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1'),'my-dj-auth.herokuapp.com']
 
 # Application definition
 
