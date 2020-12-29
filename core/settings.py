7d409351fcece02b8cb614d2fbcce9ec9dc2146c
anaco <ndedaanaco@gmail.com>
2020-12-29 14:31:17 +0300

    added two buttons

--- core/settings.py
+++ core/settings.py
@@ -96,9 +96,9 @@ DATABASES = {
 }
 
 # heroku db config
-import dj_database_url
-
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# import dj_database_url
+#
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
